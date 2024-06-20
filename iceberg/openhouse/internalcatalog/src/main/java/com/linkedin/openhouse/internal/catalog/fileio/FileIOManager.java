7f3e95a9bf75c5cba898a6beb5a0139ed28f6bfa
kmcclenn <kmcclenn@mit.edu>
2024-06-20 12:42:38 -0700

    removed s3 references so tests/build passes

--- iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/fileio/FileIOManager.java
+++ iceberg/openhouse/internalcatalog/src/main/java/com/linkedin/openhouse/internal/catalog/fileio/FileIOManager.java
@@ -11,7 +11,6 @@ import com.linkedin.openhouse.cluster.storage.hdfs.HdfsStorage;
 import com.linkedin.openhouse.cluster.storage.local.LocalStorage;
 import java.util.Optional;
 import java.util.function.Supplier;
-import org.apache.iceberg.aws.s3.S3FileIO;
 import org.apache.iceberg.azure.adlsv2.ADLSFileIO;
 import org.apache.iceberg.hadoop.HadoopFileIO;
 import org.apache.iceberg.io.FileIO;
@@ -38,16 +37,11 @@ public class FileIOManager {
   FileIO localFileIO;
 
   @Autowired(required = false)
-  S3FileIO s3FileIO;
-
-  @Autowired(required = false)
   ADLSFileIO adlsFileIO;
 
   @Autowired HdfsStorage hdfsStorage;
 
   @Autowired LocalStorage localStorage;
-
-  @Autowired S3Storage s3Storage;
 
   @Autowired AdlsStorage adlsStorage;
 
@@ -65,8 +59,6 @@ public class FileIOManager {
       return Optional.ofNullable(hdfsFileIO).orElseThrow(exceptionSupplier);
     } else if (LOCAL.equals(storageType)) {
       return Optional.ofNullable(localFileIO).orElseThrow(exceptionSupplier);
-    } else if (S3.equals(storageType)) {
-      return Optional.ofNullable(s3FileIO).orElseThrow(exceptionSupplier);
     } else if (ADLS.equals(storageType)) {
       return Optional.ofNullable(adlsFileIO).orElseThrow(exceptionSupplier);
     } else {
@@ -86,8 +78,6 @@ public class FileIOManager {
       return hdfsStorage;
     } else if (fileIO.equals(localFileIO)) {
       return localStorage;
-    } else if (fileIO.equals(s3FileIO)) {
-      return s3Storage;
     } else if (fileIO.equals(adlsFileIO)) {
       return adlsStorage;
     } else {
