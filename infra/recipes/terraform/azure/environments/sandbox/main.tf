87a78b742a8c25301a6e68bed6611b4f0cdb97e7
kmcclenn <kmcclenn@mit.edu>
2024-07-17 10:58:22 -0700

    debugging helm deployment

--- infra/recipes/terraform/azure/environments/sandbox/main.tf
+++ infra/recipes/terraform/azure/environments/sandbox/main.tf
@@ -3,8 +3,25 @@ resource "azurerm_resource_group" "openhouse_sandbox" {
     location = var.resource_group_location
 }
 
+resource "random_string" "storage_name" {
+    length = 5
+    special = false
+    lower = true
+    upper = false
+}
+
+locals {
+    storage_account_name = "openhousestorage${random_string.storage_name.result}" // added random string of numbers to make it unique
+    container_name = "blobcontainer"
+    db_username = "azureadmin"
+    db_password = "Pa33word"
+    db_name = "openhouse-sandbox-db"
+    db_server_name = "openhouse-sandbox-mysql-server"
+}
+
 module "vm" {
     source = "../../modules/vm"
+    depends_on = [ azurerm_resource_group.openhouse_sandbox ]
     virtual_network_name = "openhouse-sandbox-network"
     resource_group_name = var.resource_group_name
     subnet_name = "openhouse-sandbox-subnet"
@@ -18,33 +35,20 @@ module "mysql" {
     server_name = local.db_server_name
     db_admin_login = local.db_username
     db_admin_password = local.db_password
-    db_name = var.db_name
+    db_name = local.db_name
 }
 
 module "k8s" {
   source = "../../modules/k8s"
-  k8s_cluster_name = "openhouse-sandbox-k8s"
+  depends_on = [ azurerm_resource_group.openhouse_sandbox ]
   resource_group_name = azurerm_resource_group.openhouse_sandbox.name
   node_count = 1
   vm_size = "Standard_D2s_v3"
 }
 
-resource "random_string" "storage_name" {
-    length = 5
-    special = false
-}
-
-locals {
-    storage_account_name = "openhousestorage${random_string.storage_name.result}" // added random string of numbers to make it unique
-    container_name = "blobcontainer"
-    db_username = "azureadmin"
-    db_password = "Pa33word"
-    db_name = "openhouse-sandbox-db"
-    db_server_name = "openhouse-sandbox-mysql-server"
-}
-
 module "storage" {
     source = "../../modules/storage"
+    depends_on = [ azurerm_resource_group.openhouse_sandbox ]
     storage_account_name = local.storage_account_name
     resource_group_name = azurerm_resource_group.openhouse_sandbox.name
     container_name = local.container_name
